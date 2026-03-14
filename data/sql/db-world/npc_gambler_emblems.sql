bc0c094844d9d2d69997fc8389313c2916bf6710
Sebastian Chitiva <manuchitivar@gmail.com>
2026-03-14 00:21:20 -0500

    correcion del script

--- data/sql/db-world/npc_gambler_emblems.sql
+++ data/sql/db-world/npc_gambler_emblems.sql
@@ -10,7 +10,7 @@
 -- NPC
 DELETE FROM `creature_template` WHERE (`entry` = 601021);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
-(601021, 0, 0, 0, 0, 0, 'Manolo', 'Emblems', 'LootAll', 0, 80, 80, 0, 35, 1, 1, 1.14286, 1, 1, 1, 1, 4, 0, 1, 0, 0, 0, 0, 1, 4, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 16777216, 'gamble_npc', 0);
+(601021, 0, 0, 0, 0, 0, 'Manolo', 'Emblems', 'LootAll', 0, 80, 80, 0, 35, 1, 1, 1.14286, 1, 1, 1, 1, 4, 0, 1, 0, 0, 0, 0, 1, 4, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 16777216, 'gamble_npc_emblems', 0);
 -- NPC Model
 DELETE FROM `creature_template_model` WHERE `CreatureID` IN (601021);
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
